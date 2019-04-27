7bf8298fa8a9ba0519a9535414a2bc8348026b1e
Hunter Houston <hunter.houston@meltwater.com>
2019-04-27 15:57:41 -0700

    Add dropzone component to /admin

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,4 @@
-const { override, fixBabelImports, addLessLoader } = require('customize-cra');
+const { override, fixBabelImports, addLessLoader } = require('customize-cra')
 
 module.exports = override(
     fixBabelImports('import', {
@@ -9,5 +9,5 @@ module.exports = override(
     addLessLoader({
         javascriptEnabled: true,
         modifyVars: { '@primary-color': '#fb8c00' },
-    }),
-);
+    })
+)
