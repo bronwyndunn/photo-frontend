779b4f94ed586c75a856c6ca094bc38267275c78
Bronwyn Dunn <bdunn@mbp15jschapiro.otcorp.opentable.com>
2019-05-10 13:26:03 -0700

    remove from cart

--- src/components/Stripe/CheckoutFormContainer.js
+++ src/components/Stripe/CheckoutFormContainer.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 
 import StripeProviderForm from './StripeProviderForm'
 import Cart from './Cart'
-import { clearCart } from '../../actions/player'
+import { clearCart, removeItemFromCart } from '../../actions/player'
 
 
 class CheckoutFormContainer extends Component {
@@ -26,7 +26,8 @@ const mapStateToProps = (state) => ({
 })
 
 const mapDispatchToProps = dispatch => ({
-  clearCart: () => dispatch(clearCart())
+  clearCart: () => dispatch(clearCart()),
+  removeItemFromCart: (photoId) => dispatch(removeItemFromCart(photoId))
 })
 
 export default connect(
